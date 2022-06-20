610ed19f7098555d7373f4e5fb87fa9011d52b96
Ping <pingping.sim83@gmail.com>
2022-06-20 11:00:08 +0800

    Setup dashboard bottom navigation bar

--- app/src/main/java/com/hanai/jiwa/ui/component/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/hanai/jiwa/ui/component/dashboard/DashboardActivity.kt
@@ -1,14 +1,21 @@
 package com.hanai.jiwa.ui.component.dashboard
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.hanai.jiwa.R
 import com.hanai.jiwa.databinding.ActivityDashboardBinding
 import com.hanai.jiwa.ui.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DashboardActivity : BaseActivity() {
     private lateinit var binding: ActivityDashboardBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initBottomNavView()
     }
 
     override fun initViewBinding() {
@@ -18,5 +25,13 @@ class DashboardActivity : BaseActivity() {
     }
 
     override fun observeViewModel() {
+    }
+
+    private fun initBottomNavView() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        //binding.navView.itemIconTintList = null
     }
 }
